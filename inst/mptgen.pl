bb827f4ce6909ce392012a2a4c6c75b687f176c4
Martin Smith <martins@gmail.com>
2018-11-20 08:59:20 +0000

    Code for reproducability

---
+++ inst/mptgen.pl
@@ -0,0 +1,34 @@
+# Runs mpts.run for files 001 > 100
+print "\r\nmptgen.pl: generate most parsimonious trees using TNT";
+# TNT does not find optimal trees with k=2 on: 100_20_6, 100_20_8, 100_44_3
+$dir = "C:/Research/implied_weight/oreilly";
+
+open (TEMPLATE, "<$dir/tntscript/template.run") or warn "ERROR: can't find template file";
+@template = <TEMPLATE>;
+close TEMPLATE;
+
+
+foreach my $rep (1..10) {
+  foreach my $nchar (100, 350, 1000) {
+    foreach my $rate (1..100) {
+      $fileno = $nchar . "_" . $rate . "_" . $rep;
+      if (-e "$dir/Trees/k200.$fileno.so20.con.tre") {
+        print "\nSkipping $fileno: results already exist for k200, so20";
+      } else {
+        print "\n" . localtime . ": Processing $fileno...";
+        $scriptfile = "$dir/tntscript/run$fileno.run";
+        open (SCRIPT, ">", $scriptfile) or warn "Can't open script file";
+        for (@template) {
+          $line = $_;
+          $line =~ s/%1/$fileno/g;
+          print SCRIPT $line;
+        }
+        close SCRIPT;
+        system("tnt_cmd \"proc $scriptfile\";"); # tnt_cmd is a copy of the COMMAND LINE version of tnt
+        # To run TNT using the system command it is necessary (in Windows) to add its containing 
+        # folder to the System Path, under 'Environment Variables'
+      }
+    }
+  }
+}
+do "tnt2nex.pl";
