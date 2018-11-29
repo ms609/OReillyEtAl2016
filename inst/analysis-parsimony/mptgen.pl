26d2e641af20fe8da3ff4cc37e364dcacb39581d
Martin Smith <martins@gmail.com>
2018-11-29 13:59:33 +0000

    Streamline parsimony analysis files

--- inst/analysis-parsimony/mptgen.pl
+++ inst/analysis-parsimony/mptgen.pl
@@ -14,20 +14,21 @@ close TEMPLATE;
 foreach my $rep (1..10) {
   foreach my $nchar (100, 350, 1000) {
     foreach my $rate (1..100) {
-      $fileno = $nchar . "_" . $rate . "_" . $rep;
-      if (-e "$dir/Trees/k200.$fileno.so20.con.tre") {
-        print "\nSkipping $fileno: results already exist for k200, so20";
+      $fileno = $nchar . "/" . $rate . "_" . $rep;
+      if (-e "$dir/Trees/k200.$fileno.sym") {
+        print "\nSkipping $fileno: results already exist for k200";
       } else {
         print "\n" . localtime . ": Processing $fileno...";
-        $scriptfile = "$dir/tntscript/run$fileno.run";
+        $scriptfile = "$dir/temp_tnt_script.$nchar.$rate.$rep.run";
         open (SCRIPT, ">", $scriptfile) or warn "Can't open script file";
         for (@template) {
           $line = $_;
-          $line =~ s/%1/$fileno/g;
+          $line =~ s/%1/$nchar\/$rate-$rep/g;
           print SCRIPT $line;
         }
         close SCRIPT;
         system("tnt_cmd \"proc $scriptfile\";"); # tnt_cmd is a copy of the COMMAND LINE version of tnt
+        unlink($scriptfile);
         # To run TNT using the system command it is necessary (in Windows) to add its containing 
         # folder to the System Path, under 'Environment Variables'
       }
